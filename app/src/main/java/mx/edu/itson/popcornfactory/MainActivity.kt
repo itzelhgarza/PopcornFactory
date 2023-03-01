baa1420f62f15b59236dbc6f022bde137323dc9b
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2023-03-01 15:21:11 -0700

    Se supone que ya

--- app/src/main/java/mx/edu/itson/popcornfactory/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/popcornfactory/MainActivity.kt
@@ -1,11 +1,19 @@
 package mx.edu.itson.popcornfactory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button: Button = findViewById(R.id.boton)
+
+        button.setOnClickListener{
+            var intento = Intent(this, CatalogActivity::class.java)
+        }
     }
 }
---
+++ app/src/main/java/mx/edu/itson/popcornfactory/Pelicula.kt
@@ -0,0 +1,3 @@
+package mx.edu.itson.popcornfactory
+
+data class Pelicula(var titulo: String, var image: Int, var header: Int, var sinopsis: String)
